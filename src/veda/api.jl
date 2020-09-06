5e642d119f9c802df9d4605de5ce768e5e08e3dc
Valentin Churavy <vchuravy@csail.mit.edu>
2020-09-06 18:19:02 +0200

    find libveda.so

--- src/veda/api.jl
+++ src/veda/api.jl
@@ -1,5 +1,12 @@
 module API
     using CEnum
+    using Libdl
+
+    # TODO: This is quite simplistic, better create a build of VEDA through Yggdrasil
+    const libveda = Libdl.find_library("libveda.so", ["/usr/local/ve/veda/lib64/"])
+    if isempty(libveda)
+        @warn "Could not find libveda, VectorEngine.jl will not function"
+    end
 
     # TODO: figure out why Clang.jl missed these
     const __VEDAcontext = Cvoid
