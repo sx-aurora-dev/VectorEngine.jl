c113a5c75604b14cd24339a5adf9cc13df480717
Valentin Churavy <vchuravy@csail.mit.edu>
2020-09-08 13:02:50 +0200

    Allow calling all runtime functions

--- src/compiler.jl
+++ src/compiler.jl
@@ -3,3 +3,9 @@ struct VECompilerParams <: AbstractCompilerParams end
 const VECompilerJob = CompilerJob{VECompilerTarget, VECompilerParams}
 
 GPUCompiler.runtime_module(::VECompilerJob) = VectorEngine
+
+GPUCompiler.isintrinsic(job::VECompilerJob, fn::String) =
+    invoke(GPUCompiler.isintrinsic,
+           Tuple{CompilerJob{VECompilerTarget}, typeof(fn)},
+           job, fn) ||
+    true
