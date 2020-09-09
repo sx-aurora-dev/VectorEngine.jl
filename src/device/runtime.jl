f5e7e00a47c18820434c97657a95b403eed54400
Erich Focht <efocht@gmail.com>
2020-09-09 13:01:05 +0200

    Adding some output to runtime functions reporting errors and exceptions.

--- src/device/runtime.jl
+++ src/device/runtime.jl
@@ -8,17 +8,27 @@ function signal_exception()
 end
 
 function report_exception(ex)
+    @veprintf("""
+        ERROR: a %s was thrown during kernel execution.
+        Run Julia on debug level 2 for device stack traces.
+        """, ex)
     return
 end
 
 function report_oom(sz)
+    @veprintf("ERROR: Out of dynamic VE memory (trying to allocate %i bytes)\n", sz)
     return
 end
 
 function report_exception_name(ex)
+    @veprintf("""
+        ERROR: a %s was thrown during kernel execution.
+        Stacktrace:
+        """, ex)
     return
 end
 
 function report_exception_frame(idx, func, file, line)
+    @veprintf(" [%i] %s at %s:%i\n", idx, func, file, line)
     return
 end
