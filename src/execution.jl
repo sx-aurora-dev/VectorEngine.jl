90c25cbfc7cbc17a744fca95fd276a9fcec309fd
Erich Focht <efocht@gmail.com>
2022-02-10 17:07:30 +0100

    Adapted to cheges in GPUCompiler.

--- src/execution.jl
+++ src/execution.jl
@@ -35,37 +35,106 @@ end
 
 isghosttype(dt) = !dt.mutable && sizeof(dt) == 0
 
+# old # function vefunction(f::Core.Function, tt::Type=Tuple{}; name=nothing, kernel=true, kwargs...)
+# old #     source = FunctionSpec(f, tt, kernel, name)
+# old #     GPUCompiler.cached_compilation(_vefunction, source; kwargs...)::HostKernel{f,tt}
+# old # end
+# old # 
+# old # # actual compilation
+# old # function _vefunction(source::FunctionSpec; kwargs...)
+# old #     # compile to GCN
+# old #     target = VECompilerTarget()
+# old #     params = VECompilerParams()
+# old #     job = CompilerJob(target, source, params)
+# old #     obj, kernel_fn, undefined_fns, undefined_gbls = GPUCompiler.compile(:obj, job)
+# old # 
+# old #     # create executable and kernel
+# old #     obj = codeunits(obj)
+# old #     mod = VEModule(obj)
+# old #     fun = VEFunction(mod, kernel_fn)
+# old #     kernel = HostKernel{source.f,source.tt}(mod, fun)
+# old # 
+# old #     return kernel
+# old # end
+
 """
     vefunction(f, tt=Tuple{}; kwargs...)
 
-Low-level interface to compile a function invocation, returning
-a callable kernel object.
-- `name`: override the name that the kernel will have in the generated code
-The output of this function is automatically cached, i.e. you can simply call `vefunction`
-in a hot path without degrading performance. New code will be generated automatically, when
-when function changes, or when different types or keyword arguments are provided.
-"""
-function vefunction(f::Core.Function, tt::Type=Tuple{}; name=nothing, kernel=true, kwargs...)
-    source = FunctionSpec(f, tt, kernel, name)
-    GPUCompiler.cached_compilation(_vefunction, source; kwargs...)::HostKernel{f,tt}
-end
+Low-level interface to compile a function invocation for the currently-active GPU, returning
+a callable kernel object. For a higher-level interface, use [`@roc`](@ref).
 
-# actual compilation
-function _vefunction(source::FunctionSpec; kwargs...)
-    # compile to GCN
+The following keyword arguments are supported:
+- `name`: overrides the name that the kernel will have in the generated code
+- `device`: chooses which device to compile the kernel for
+- `global_hooks`: specifies maps from global variable name to initializer hook
+
+The output of this function is automatically cached, i.e. you can simply call `rocfunction`
+in a hot path without degrading performance. New code will be generated automatically, when
+function definitions change, or when different types or keyword arguments are provided.
+"""
+function vefunction(f::Core.Function, tt::Type=Tuple{}; name=nothing, device=0, global_hooks=NamedTuple(), kwargs...)
+    source = FunctionSpec(f, tt, true, name)
+    cache = get!(()->Dict{UInt,Any}(), vefunction_cache, device)
+    #isa = default_isa(device)
     target = VECompilerTarget()
     params = VECompilerParams()
     job = CompilerJob(target, source, params)
-    obj, kernel_fn, undefined_fns, undefined_gbls = GPUCompiler.compile(:obj, job)
+    GPUCompiler.cached_compilation(cache, job, vefunction_compile, vefunction_link)::HostKernel{f,tt}
+end
+
+const vefunction_cache = Dict{UInt,Dict{UInt,Any}}()
+
+
+function vefunction_compile(@nospecialize(job::CompilerJob))
+    # compile
+    method_instance, mi_meta = GPUCompiler.emit_julia(job)
+    ir, ir_meta = GPUCompiler.emit_llvm(job, method_instance)
+    kernel = ir_meta.entry
+
+    obj, obj_meta = GPUCompiler.emit_asm(job, ir; format=LLVM.API.LLVMObjectFile)
+
+    # find undefined globals and calculate sizes
+    globals = map(gbl->Symbol(LLVM.name(gbl))=>llvmsize(eltype(llvmtype(gbl))),
+                  filter(x->isextinit(x), collect(LLVM.globals(ir))))
+    entry = LLVM.name(kernel)
+    dispose(ir)
+
+    return (;obj, entry, globals)
+end
+function vefunction_link(@nospecialize(job::CompilerJob), compiled)
+    device = job.params.device
+    global_hooks = job.params.global_hooks
+    obj, entry, globals = compiled.obj, compiled.entry, compiled.globals
 
     # create executable and kernel
     obj = codeunits(obj)
-    mod = VEModule(obj)
-    fun = VEFunction(mod, kernel_fn)
-    kernel = HostKernel{source.f,source.tt}(mod, fun)
+    #exe = create_executable(device, entry, obj; globals=globals)
+    mod = VEModule(exe)
+    fun = VEFunction(mod, entry)
+    kernel = HostKernel{job.source.f,job.source.tt}(mod, fun)
+
+    # initialize globals from hooks
+    for gname in first.(globals)
+        hook = nothing
+        if haskey(default_global_hooks, gname)
+            hook = default_global_hooks[gname]
+        elseif haskey(global_hooks, gname)
+            hook = global_hooks[gname]
+        end
+        if hook !== nothing
+            @debug "Initializing global $gname"
+            gbl = get_global(exe, gname)
+            hook(gbl, mod, device)
+        else
+            @debug "Uninitialized global $gname"
+            continue
+        end
+    end
 
     return kernel
 end
+
+
 
 # https://github.com/JuliaLang/julia/issues/14919
 (kernel::HostKernel)(args...; kwargs...) = call(kernel, args...; kwargs...)
