ddcf84f2fb7156177a5d57a6d1d8c3b868abdac6
Erich Focht <efocht@gmail.com>
2022-02-18 23:18:58 +0100

    Example for a device array kernel. This needs @veda to be implemented.

---
+++ examples/vadd.jl
@@ -0,0 +1,25 @@
+using VectorEngine, Test
+
+function vadd(a, b, c)
+    for i = 1:prod(size(a))
+        @inbounds c[i] = a[i] + b[i]
+    end
+    return
+end
+
+dims = (2,)
+a = round.(rand(Float32, dims) * 100)
+b = round.(rand(Float32, dims) * 100)
+c = similar(a)
+
+d_a = VEArray(a)
+d_b = VEArray(b)
+d_c = VEArray(c)
+
+len = prod(dims)
+
+@veda items=len vadd(d_a, d_b, d_c)
+VectorEngine.vesync()
+
+c = Array(d_c)
+@test a+b ≈ c
