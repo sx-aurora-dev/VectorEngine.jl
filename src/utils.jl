33eced7954045c7e6ef806f74854b8c14051f808
Erich Focht <efocht@gmail.com>
2022-02-24 02:51:08 +0100

    Forgot this occurence of vesync. Replaced with synchronize.

--- src/utils.jl
+++ src/utils.jl
@@ -6,12 +6,12 @@ export @sync
 
 Run expression `ex` and synchronize the VE afterwards.
 
-See also: [`vesync`](@ref).
+See also: [`synchronize`](@ref).
 """
 macro sync(ex)
     quote
         local ret = $(esc(ex))
-        VectorEngine.vesync()
+        VectorEngine.synchronize()
         ret
     end
 end
